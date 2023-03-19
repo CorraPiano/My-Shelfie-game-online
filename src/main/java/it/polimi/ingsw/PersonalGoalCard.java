6f48ea916d6430a49565e49c251799b13c5d307f
nicola-de-march <81023350+nicola-de-march@users.noreply.github.com>
2023-03-19 18:11:39 +0100

    Implementata parzialmente PersonalGoalCard con ADT

---
+++ src/main/java/it/polimi/ingsw/PersonalGoalCard.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw;
+
+import java.util.Random;
+
+public class PersonalGoalCard {
+    private final DataCard card;
+    public PersonalGoalCard() {
+        this.card = new DataCard(new Random().nextInt(11));
+    }
+    public int calculatePoints(){
+        return 0;
+    }
+
+    public DataCard getCard() {
+        return card;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/commoncard/CommonGoalCard12.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.commoncard;
+
+public class CommonGoalCard12 {
+}
