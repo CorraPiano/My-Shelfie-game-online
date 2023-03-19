f3122bc0f76b45c9e53f566a41cf3d25479100b1
flaviodelellis <flavio.delellis@mail.polimi.it>
2023-03-19 19:53:25 +0100

    Importo libreria per package CommonGoalCard

--- src/main/java/it/polimi/ingsw/BagCommon.java
+++ src/main/java/it/polimi/ingsw/BagCommon.java
@@ -1,12 +1,16 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.commoncard.CommonGoalCard;
+
 public class BagCommon extends Bag{
 
     private CommonGoalCard commonCard;
 
 
-    public void initializeBag() {};
+    public void initializeBag() { }; //
 
     public CommonGoalCard getCard(){};
 
+
 }
+
