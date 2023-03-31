8229b670d70d7856aa4f006c68366f7a35626e32
C0NN <79844427+C0NN@users.noreply.github.com>
2023-03-31 19:06:52 +0200

    Partially implemented the classes GameMode, Gameplay and PlayerIterator

---
+++ src/main/java/it/polimi/ingsw/model/Gameplay.java
@@ -0,0 +1,107 @@
+package it.polimi.ingsw.model;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+
+public class Gameplay {
+
+    GameMode gameMode;
+    int numPlayers;
+    ArrayList<Player> playerList = new ArrayList<>();
+    Board board;
+    CommonGoalCard commonGoalCard1;
+    CommonGoalCard commonGoalCard2;
+    BagPersonal bagPersonal;
+    BagCommon bagCommon;
+    PlayerIterator playerIterator;
+    ArrayList<Item> hand;
+    Player winner;
+    Player currentPlayer;
+
+    public Gameplay(GameMode gameMode, int numPlayers){
+        this.gameMode=gameMode;
+        this.numPlayers=numPlayers;
+        board=new Board();
+        bagPersonal=new BagPersonal();
+        if (gameMode.equals(GameMode.EXPERT)) {
+            bagCommon=new BagCommon();
+        }
+        hand = new ArrayList<Item>();
+    }
+
+    public Player addPlayer(String name){
+        Player player = new Player(name);
+        playerList.add(player);
+        return player;
+    }
+
+    public PlayerIterator startGame(){
+        if (gameMode.equals(GameMode.EXPERT)){
+            // #1 implementare getCard() in BagCommon
+            // commonGoalCard1 = bagCommon.getCard();
+            // commonGoalCard2 = bagCommon.getCard();
+        }
+        for(Player p: playerList) {
+            // #2 implementare getCard() in BagPersonal
+            // #3 implementare givePersonalGoalCard in Player
+            //Player.givePersonalGoalCard(bagPersonal.getCard());
+        }
+        playerIterator = new PlayerIterator(playerList);
+        return playerIterator;
+    }
+
+    // #4 sistemare le eccezioni
+    public void pickItems(ArrayList<Coordinates> list) throws Exception{
+        hand.addAll(board.getItemList(list));
+    }
+
+    public void releaseItems() {
+        // #5 implementare putItems in board
+        // board.putItems(hand);
+        hand.clear();
+    }
+
+    public int putItems(int column) throws Exception{
+        Bookshelf library = currentPlayer.getLibrary();
+        library.putItemList(hand,column);
+        if(library.isFull())
+            playerIterator.notifyLastRound();
+        return calcPoints();
+    }
+
+    public int calcPoints(){
+        // #6 in player mettere boolean haveToken() al posto di getToken
+        /* if(!currentPlayer.haveToken1()){
+            if(commonGoalCard1.checkFullfill(currentPlayer.getLibrary()))
+                currentPlayer.setToken1(commonGoalCard1.popToken());
+        }
+        if(!currentPlayer.haveToken2()){
+            if(commonGoalCard2.checkFullfill(currentPlayer.getLibrary()))
+                currentPlayer.setToken2(commonGoalCard2.popToken());
+        }*/
+        currentPlayer.updatePoints(false);
+        return currentPlayer.getPoints();
+    }
+
+    public Player endGame(){
+
+        // #6 in player mettere boolean haveFirstPlayerSeat()
+        for(Player p: playerList) {
+            p.updatePoints(true);
+        }
+        playerList=playerList.stream().sorted((x,y)->{
+            if(x.getPoints()<y.getPoints())
+                return 1;
+            /*if(x.getPoints()==y.getPoints() && x.haveFirstPlayerSeat())
+                return 1;
+            if(x.getPoints()==y.getPoints() && y.haveFirstPlayerSeat())
+                return -1;*/
+            else
+                return -1;
+        }).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        return playerList.get(0);
+    }
+}
+
+
+
---
+++ src/main/java/it/polimi/ingsw/model/PlayerIterator.java
@@ -0,0 +1,46 @@
+package it.polimi.ingsw.model;
+
+import java.util.ArrayList;
+
+public class PlayerIterator {
+    private int size;
+    private int curr;
+    private boolean lastRound;
+    private int remains;
+    private final ArrayList<Player> playerList;
+
+    public PlayerIterator(ArrayList<Player> playerList){
+        this.playerList=playerList;
+        size = playerList.size();
+        curr = (int)(Math.random()*(size+1));
+    }
+
+    public Player current(){
+        return playerList.get(curr);
+    }
+
+    public void next() throws Exception{
+        if(lastRound) {
+            if (remains <= 0)
+                throw new Exception();
+            remains = remains - 1;
+        }
+
+        if(curr<size-1)
+            curr=curr+1;
+        else
+            curr=0;
+    }
+
+    // non ritorna eccezioni, ma ignora la notifica se è stata gia inviata
+    public void notifyLastRound(){
+        if(!lastRound) {
+            lastRound = true;
+            remains = size - 1;
+        }
+    }
+
+    public boolean isEnd(){
+        return lastRound && remains<=0;
+    }
+}
