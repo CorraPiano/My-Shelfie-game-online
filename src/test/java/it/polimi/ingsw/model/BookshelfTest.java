52a1cc06b198d35883817e1b921b934489c2df9d
nicola-de-march <81023350+nicola-de-march@users.noreply.github.com>
2023-04-13 16:11:10 +0200

    PersonalGoalCard testata e sistemata

--- src/test/java/it/polimi/ingsw/model/BookshelfTest.java
+++ src/test/java/it/polimi/ingsw/model/BookshelfTest.java
@@ -10,26 +10,27 @@ import it.polimi.ingsw.model.util.InputTest;
 
 class BookshelfTest {
     // ATTRIBUTES
-    private Bookshelf bookshelf;
+    private Bookshelf bookshelf = new Bookshelf();
+    // private Bookshelf bookshelfTest = new Bookshelf();
     private ArrayList<Item> itemList1 = new ArrayList<>();
     private ArrayList<Item> itemList2 = new ArrayList<>();
 
 
     // METHODS
     private void createItemList() {
-        ItemType type1 = ItemType.GREEN;
-        ItemType type2 = ItemType.YELLOW;
-        ItemType type3 = ItemType.BLUE;
-        ItemType type4 = ItemType.PINK;
-        ItemType type5 = ItemType.CYAN;
-        ItemType type6 = ItemType.WHITE;
+        ItemType type0 = ItemType.GREEN;
+        ItemType type1 = ItemType.YELLOW;
+        ItemType type2 = ItemType.BLUE;
+        ItemType type3 = ItemType.PINK;
+        ItemType type4 = ItemType.CYAN;
+        ItemType type5 = ItemType.WHITE;
 
-        Item item1 = new Item(type1);
-        Item item2 = new Item(type2);
-        Item item3 = new Item(type3);
-        Item item4 = new Item(type4);
-        Item item5 = new Item(type5);
-        Item item6 = new Item(type6);
+        Item item1 = new Item(type0);
+        Item item2 = new Item(type1);
+        Item item3 = new Item(type2);
+        Item item4 = new Item(type3);
+        Item item5 = new Item(type4);
+        Item item6 = new Item(type5);
 
         itemList1.add(item1);
         itemList1.add(item2);
@@ -39,31 +40,108 @@ class BookshelfTest {
         itemList2.add(item1);
     }
 
+    private int counterTotalValue(Bookshelf bookshelf) {
+        int value = 0;
+        Coordinates coordinates = new Coordinates();
+        for(int row=0; row<6; row++) {
+            for(int column=0; column<5; column++) {
+                coordinates.setColumn(column);
+                coordinates.setRow(row);
+                if(bookshelf.getItem(coordinates).isPresent()){
+                    value = value + bookshelf.getItem(coordinates).get().getType().getValue();
+                }
+            }
+        }
+        return value;
+    }
+
+    /*
+    // funzione realizzata per cercare di utilizzare la putItemList con i file.txt ma c'è il problema degli oggetti a null
+    private int itemValueAtCoordinates(int index, Coordinates coordinates) {
+        InputTest input = TestFactory.createTest();
+        if(input.getInputLibrary(index).getItem(coordinates).isPresent()){
+            return input.getInputLibrary(index).getItem(coordinates).get().getType().getValue();
+
+        }
+        else {
+            return 9;
+        }
+    }
+     */
+
     // TESTS
     @Test
-    void putItemList() throws Exception {
-        Bookshelf bookshelf = new Bookshelf();
-        bookshelf.putItemList(itemList1, 1);
-        // dovrebbe funzionare, non ho ancora controllato bene
-    }
-
-    @Test
-    void getItem() {
-        // TODO
-    }
-
-    @Test
     void calculatePoints() throws Exception {
+        // carico le matrici dal file BookshelfTestFile_updatePoints.txt in InputTest
+        InputTest input = TestFactory.createTest("src/test/java/it/polimi/ingsw/model/util/BookshelfTestFile_putItemList.txt");
 
-        // carico le matrici dal file BookshelfTestFile.txt in InputTest
-        InputTest input = TestFactory.createTest();
-
-        // problemi con la matrice vuota forse perchè il -1 non va bene come numero
-        // TODO: Assertions.assertEquals(input.getResult(0), input.getInputLibrary(0).calculatePoints());
-
-        // testo tutte le matrici del file BookshelfTestFile.txt
-        for(int i = 1; i < input.numberOfTests(); i++) {
+        // testo tutte le matrici del file BookshelfTestFile_updatePoints.txt
+        for(int i = 0; i < input.numberOfTests(); i++) {
             Assertions.assertEquals(input.getResult(i), input.getInputLibrary(i).calculatePoints());
         }
     }
+
+    /*
+    @Test
+    // test che utilizza i file.txt ma c'è il problema degli oggetti a null
+    void putItemList1() throws Exception {
+        // carico le matrici dal file BookshelfTestFile_putItemList.txt in InputTest
+        InputTest input = TestFactory.createTest();
+
+        input.getInputLibrary(0).putItemList(itemList1, 1);
+        Coordinates coordinates = new Coordinates();
+
+        // inserimento di 3 item in libreria piena
+        for(int row=0; row<6; row++) {
+            for(int column=0; column<5; column++) {
+                coordinates.setColumn(column);
+                coordinates.setRow(row);
+                Assertions.assertEquals(itemValueAtCoordinates(1, coordinates), itemValueAtCoordinates(0, coordinates));
+                System.out.print(itemValueAtCoordinates(0, coordinates) + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+     */
+
+    @Test
+    void putItemList() throws Exception {
+
+        // creo le liste di Item da inserire
+        createItemList();
+        // inserisco gli Item
+        bookshelf.putItemList(itemList1, 1);
+        bookshelf.putItemList(itemList1, 2);
+        bookshelf.putItemList(itemList2, 2);
+        bookshelf.putItemList(itemList1, 4);
+
+        // Stampo la matrice per vedfere se effettivamente li ha inseriti correttamente
+        Coordinates coordinates = new Coordinates();
+        for(int row = 5; row >= 0; row--) {
+            for(int column = 0; column < 5; column++) {
+                coordinates.setColumn(column);
+                coordinates.setRow(row);
+                if(bookshelf.getItem(coordinates).isPresent()){
+                    System.out.print(bookshelf.getItem(coordinates).get().getType().getValue() + " ");
+                }
+                else {
+                    System.out.print("9 ");
+                    // il 9 rappresenta il valore null e in counterTotalValue() non viene contato
+                }
+            }
+            System.out.print("\n");
+        }
+
+        // Forse non ha troppo senso farla così
+        Assertions.assertEquals(12, counterTotalValue(bookshelf));
+        // Stampa della somma dei valori degli Item inseriti
+        System.out.println("Total value: "+ counterTotalValue(bookshelf));
+    }
+
+    /*
+    ==============================================================================
+    I metodi quali: noSpaceLeft(), isFull(), getItem(), putItem(), fillBookshelf()
+    sono banali o verificati di conseguenza ai test precedenti
+    ==============================================================================
+     */
 }
