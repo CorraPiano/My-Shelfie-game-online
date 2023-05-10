b3b24806f3755748a1262d34af6b3c2545e83c52
flaviodelellis <flavio.delellis@mail.polimi.it>
2023-05-10 15:54:05 +0200

    Added JavaFX libraries

--- src/main/java/it/polimi/ingsw/client/view/GUI.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI.java
@@ -1,4 +1,21 @@
 package it.polimi.ingsw.client.view;
 
-public class GUI{
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("/GUI.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+    }
+        public static void main (String[]args){
+            launch(args);
+        }
+    
 }
