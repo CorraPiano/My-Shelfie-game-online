a64eb284723a107c99ddf33c02b3e777438e6202
nicola-de-march <81023350+nicola-de-march@users.noreply.github.com>
2023-06-28 21:09:27 +0200

    update exception name

--- src/main/java/it/polimi/ingsw/exception/InvalidNameException.java
+++ src/main/java/it/polimi/ingsw/exception/InvalidNameException.java
@@ -2,6 +2,6 @@ package it.polimi.ingsw.exception;
 
 public class InvalidNameException extends Exception{
     public String toString(){
-        return "You weren't parte of the game you are trying to join";
+        return "There aren't player with this name";
     }
 }
